778cc21d3b061decae644599e6f1657aff245224
gargang2a <gargang2a@gmail.com>
2026-01-27 17:55:28 +0900

    Joystick System

--- Assets/01_Scripts/PlayerBall.cs
+++ Assets/01_Scripts/PlayerBall.cs
@@ -1,38 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBall : MonoBehaviour
 {
+    public Joystick joystick; // 조이스틱 입력 받기 위한 변수
 
     [SerializeField][Range(0f, 200f)] float speed; // 이동 스피드
     [SerializeField][Range(0f, 100f)] float jumpPower; // 점프 파워
-    public int itemCount; // 아이템 갯수
 
-    bool isJump;
-    bool inputJumpKey;
+    public int itemCount = 0; // 아이템 갯수
 
-    Rigidbody rb;
-
-    private float inputH;
-    private float inputV;
+    private Rigidbody rb;
+    private bool isGrounded;
+    private bool jumpRequested;
 
     void Awake()
     {
-        itemCount = 0;
-        isJump = false;
-        inputJumpKey = false;
         rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        inputH = Input.GetAxisRaw("Horizontal"); // 좌우 방향키에 따라 -1,0,1 의 값을 return한다.
-        inputV = Input.GetAxisRaw("Vertical");   // 상하 방향키에 따라 -1,0,1 의 값을 return한다.
+        //inputH = Input.GetAxisRaw("Horizontal"); // 좌우 방향키에 따라 -1,0,1 의 값을 return한다.
+        //inputV = Input.GetAxisRaw("Vertical");   // 상하 방향키에 따라 -1,0,1 의 값을 return한다.
 
-        if (Input.GetButtonDown("Jump") && isJump == false)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            inputJumpKey = true;
+            jumpRequested = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //h = Input.GetAxisRaw("Horizontal");
+        //v = Input.GetAxisRaw("Vertical");
+
+        // 조이스틱 입력 받기
+        float h = joystick.Direction.x;
+        float v = joystick.Direction.y;
+
+        Vector3 moveVector = new Vector3(h, 0, v);
+        rb.AddForce(moveVector * speed * Time.fixedDeltaTime, ForceMode.Impulse);
+
+        if (jumpRequested && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+
+            isGrounded = false;
+            jumpRequested = false;
         }
     }
 
@@ -40,7 +55,7 @@ public class PlayerBall : MonoBehaviour
     {
         if (collision.gameObject.tag == "Floor")
         {
-            isJump = false;
+            isGrounded = true;
         }
     }
 
@@ -52,24 +67,18 @@ public class PlayerBall : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
-    {
-        Vector3 moveVector = new Vector3(inputH, 0, inputV);
-        rb.AddForce(moveVector * speed * Time.fixedDeltaTime, ForceMode.Impulse);
-
-        if (inputJumpKey)
-        {
-            isJump = true;
-            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-
-            inputJumpKey = false;
-        }
-    }
-
     public void AddItem(int count = 1)
     {
         itemCount++;
         Debug.Log("Item Count:" + itemCount);
+    }
+
+    // 모바일 점프 버튼
+    public void OnJumpButton()
+    {
+        if (isGrounded)
+        {
+            jumpRequested = true;
+        }
     }
 }
